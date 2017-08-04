a7ce0cdc6142e5de7b90fbe9a2fb9e9d422aae20
ridshah <ridshah@umail.iu.edu>
2017-08-03 22:46:41 -0400

    feat(module-api):	add controller files

---
+++ rest/src/main/java/io/myapp/api/constants/URI.java
@@ -0,0 +1,13 @@
+package io.myapp.api.constants;
+
+public final class URI {
+	public static final String WEATHER = "weathers";
+
+	public static final String CITY = "/city/{city}";
+	
+	public static final String CITYANDPROPERTY = "/city/{city}/property/{property}";
+	
+	public static final String CITYAVERAGEBYHOUR = "/city/{city}/hourly";
+	
+	public static final String CITYAVERAGEBYDAY = "/city/{city}/daily";
+}
